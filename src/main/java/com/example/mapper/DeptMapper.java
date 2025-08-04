5ba6f7d1087ef5266992cfaf9db58912d9993fa9
wuzeyu <1743824272@qq.com>
2025-08-04 15:59:54 +0800

    部门管理

---
+++ src/main/java/com/example/mapper/DeptMapper.java
@@ -0,0 +1,25 @@
+package com.example.mapper;
+
+import com.example.pojo.Dept;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface DeptMapper {
+
+    @Select("SELECT id, name, create_time, updata_time from dept")
+    List<Dept> findAll();
+
+    @Delete("DELETE FROM dept WHERE id=#{id}")
+    void deleteById(Integer id);
+
+    @Insert("insert into dept(name,create_time,update_time) values(#{name},#{createTime},#{updateTime})")
+    void insert(Dept dept);
+
+    @Select("SELECT id, name, create_time, update_time from dept WHERE id=#{id}")
+    Dept getById(Integer id);
+
+    @Update("UPDATE dept SET name=#{name},update_time=#{updateTime} WHERE id=#{id}")
+    void update(Dept dept);
+}
