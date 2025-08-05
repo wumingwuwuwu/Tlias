2d9579dacb7e3d9c75da8bd326f53835b0418c92
wuzeyu <1743824272@qq.com>
2025-08-05 11:38:17 +0800

    全局异常处理

---
+++ src/main/java/com/example/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.exception;
+
+import com.example.pojo.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    //处理异常
+    @ExceptionHandler//捕获异常，通过形参指定要处理的异常类型，
+    public Result ex(Exception e){//方法形参中指定能够处理的异常类型
+        e.printStackTrace();//打印堆栈中的异常信息
+        //捕获到异常之后，响应一个标准的Result
+        return Result.error("对不起,操作失败,请联系管理员");
+    }
+
+
+}
