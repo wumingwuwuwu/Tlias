db6421c2ba4ad398995c959f2d38f5ca71c0d18a
wuzeyu <1743824272@qq.com>
2025-08-05 11:44:44 +0800

    全局异常处理（补充）

--- src/main/java/com/example/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.exception;
 
 import com.example.pojo.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,6 +16,12 @@ public class GlobalExceptionHandler {
         e.printStackTrace();//打印堆栈中的异常信息
         //捕获到异常之后，响应一个标准的Result
         return Result.error("对不起,操作失败,请联系管理员");
+    }
+
+    @ExceptionHandler//捕获异常按先子异常后父异常
+    public Result ex(DataIntegrityViolationException e){
+        e.printStackTrace();
+        return Result.error("对不起，您输出的内容过长");
     }
 
 
